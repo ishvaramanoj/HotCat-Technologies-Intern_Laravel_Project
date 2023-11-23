b25ce828d8f6f2ab6baa32c710e82b17d279c897
Ishvara <ishvaramanoj@gmail.com>
2023-11-23 15:50:35 +0530

    wwcommit

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,6 +10,7 @@ class Customer extends Model
     protected $table = 'customers';
   protected $primaryKey = 'id';
    protected $fillable = [
+        'customer_id',
         'name',
         'address',
         'age',
