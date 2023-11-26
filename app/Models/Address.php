7319c10f001e5f29f94c2a133325f15b07f37668
Ishvara <ishvaramanoj@gmail.com>
2023-11-26 09:30:38 +0530

    sscommit

---
+++ app/Models/Address.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $table = 'addresses';
+    protected $primaryKey = 'id';
+     protected $fillable = [
+          'address_one',
+          'address_two',
+          'address_three',
+      ];
+
+    use HasFactory;
+}
